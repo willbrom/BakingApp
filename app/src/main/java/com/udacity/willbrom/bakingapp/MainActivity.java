dba13b4df87abd7e17232f2518779ce7c2425424
Will <alisajid552@yahoo.com>
2017-09-27 00:26:22 +0500

    applied okhttp for network call's

--- app/src/main/java/com/udacity/willbrom/bakingapp/MainActivity.java
+++ app/src/main/java/com/udacity/willbrom/bakingapp/MainActivity.java
@@ -1,13 +1,56 @@
 package com.udacity.willbrom.bakingapp;
 
+import android.os.AsyncTask;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.udacity.willbrom.bakingapp.utilities.NetworkUtils;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String>{
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String RECIPE_URL = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+
+    @Override
+    public Loader<String> onCreateLoader(int id, Bundle args) {
+        return new AsyncTaskLoader<String>(this) {
+
+            @Override
+            protected void onStartLoading() {
+                super.onStartLoading();
+            }
+
+            @Override
+            public String loadInBackground() {
+                return null;
+            }
+
+            @Override
+            public void deliverResult(String data) {
+                super.deliverResult(data);
+            }
+        };
+    }
+
+    @Override
+    public void onLoadFinished(Loader<String> loader, String data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<String> loader) {
+
     }
 }
---
+++ app/src/main/java/com/udacity/willbrom/bakingapp/utilities/NetworkUtils.java
@@ -0,0 +1,27 @@
+package com.udacity.willbrom.bakingapp.utilities;
+
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class NetworkUtils {
+
+//    Use of okhttp to make network call and get response
+    public static String getHttpResponse(String url) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            return response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
