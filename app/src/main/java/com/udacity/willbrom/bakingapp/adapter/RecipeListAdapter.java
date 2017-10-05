817fb9290f26f7c5fcf87ee472dda9e8991b14c0
Will <alisajid552@yahoo.com>
2017-10-05 22:24:45 +0500

    added cardView and picasso

--- app/src/main/java/com/udacity/willbrom/bakingapp/adapter/RecipeListAdapter.java
+++ app/src/main/java/com/udacity/willbrom/bakingapp/adapter/RecipeListAdapter.java
@@ -1,12 +1,15 @@
 package com.udacity.willbrom.bakingapp.adapter;
 
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 import com.udacity.willbrom.bakingapp.R;
 import com.udacity.willbrom.bakingapp.model.RecipeModel;
 
@@ -28,7 +31,13 @@ public class RecipeListAdapter extends RecyclerView.Adapter<RecipeListAdapter.It
 
     @Override
     public void onBindViewHolder(ItemViewHolder holder, int position) {
-        holder.recipeItem.setText(recipeModel.get(position).getName());
+        Context context = holder.recipeThumbnail.getContext();
+        holder.recipeTitle.setText(recipeModel.get(position).getName());
+        Picasso.with(context)
+                .load("http://image.tmdb.org/t/p/w185")
+                .placeholder(R.drawable.image_placeholder)
+                .error(R.drawable.image_placeholder)
+                .into(holder.recipeThumbnail);
     }
 
     @Override
@@ -39,8 +48,8 @@ public class RecipeListAdapter extends RecyclerView.Adapter<RecipeListAdapter.It
 
     class ItemViewHolder extends RecyclerView.ViewHolder {
 
-        @BindView(R.id.recipe_item)
-        TextView recipeItem;
+        @BindView(R.id.recipe_thumbnail) ImageView recipeThumbnail;
+        @BindView(R.id.recipe_title) TextView recipeTitle;
 
         public ItemViewHolder(View itemView) {
             super(itemView);
