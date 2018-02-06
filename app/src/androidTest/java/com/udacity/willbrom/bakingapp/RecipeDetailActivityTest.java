9e75b78bcc1593cf4a905dd9bb77732eaeb996e2
willbrom <alisajid552@yahoo.com>
2018-02-06 11:50:35 +0300

    changed test

--- app/src/androidTest/java/com/udacity/willbrom/bakingapp/RecipeDetailActivityTest.java
+++ app/src/androidTest/java/com/udacity/willbrom/bakingapp/RecipeDetailActivityTest.java
@@ -7,7 +7,6 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
-
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -21,9 +20,6 @@ import org.junit.runner.RunWith;
 public class RecipeDetailActivityTest  {
 
     private static final String NUTELLA_PIE = "Nutella Pie";
-    private static final String BROWNIES = "Brownies";
-    private static final String YELLOW_CAKE = "Yellow Cake";
-    private static final String CHEESE_CAKE = "Cheesecake";
     private static final String RECIPES = "Recipe's";
 
     @Rule
@@ -36,25 +32,9 @@ public class RecipeDetailActivityTest  {
 
     @Test
     public void clickRecyclerViewItem_NutellaPie_OpensRecipeDetailActivity() {
+        onView(withId(R.id.recipe_list)).perform(RecyclerViewActions.scrollToPosition(0));
         onView(withId(R.id.recipe_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
         onView(withId(R.id.title_text_view)).check(matches(withText(NUTELLA_PIE)));
     }
 
-    @Test
-    public void clickRecyclerViewItem_Brownies_OpensRecipeDetailActivity() {
-        onView(withId(R.id.recipe_list)).perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
-        onView(withId(R.id.title_text_view)).check(matches(withText(BROWNIES)));
-    }
-
-    @Test
-    public void clickRecyclerViewItem_YellowCake_OpensRecipeDetailActivity() {
-        onView(withId(R.id.recipe_list)).perform(RecyclerViewActions.actionOnItemAtPosition(2, click()));
-        onView(withId(R.id.title_text_view)).check(matches(withText(YELLOW_CAKE)));
-    }
-
-    @Test
-    public void clickRecyclerViewItem_CheeseCake_OpensRecipeDetailActivity() {
-        onView(withId(R.id.recipe_list)).perform(RecyclerViewActions.actionOnItemAtPosition(3, click()));
-        onView(withId(R.id.title_text_view)).check(matches(withText(CHEESE_CAKE)));
-    }
 }
