ef0722b8b55e0ff354912ba3ef5d381788facc47
willbrom <alisajid552@yahoo.com>
2018-05-08 16:39:40 +0300

    Update test

--- app/src/androidTest/java/com/udacity/willbrom/bakingapp/RecipeDetailActivityTest.java
+++ app/src/androidTest/java/com/udacity/willbrom/bakingapp/RecipeDetailActivityTest.java
@@ -3,11 +3,13 @@ package com.udacity.willbrom.bakingapp;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -20,6 +22,9 @@ import org.junit.runner.RunWith;
 public class RecipeDetailActivityTest  {
 
     private static final String NUTELLA_PIE = "Nutella Pie";
+    private static final String BROWNIES = "Brownies";
+    private static final String YELLOW_CAKE = "Yellow Cake";
+    private static final String CHEESECAKE = "Cheesecake";
     private static final String RECIPES = "Recipe's";
 
     @Rule
@@ -32,9 +37,29 @@ public class RecipeDetailActivityTest  {
 
     @Test
     public void clickRecyclerViewItem_NutellaPie_OpensRecipeDetailActivity() {
-        onView(withId(R.id.recipe_list)).perform(RecyclerViewActions.scrollToPosition(0));
-        onView(withId(R.id.recipe_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(ViewMatchers.withId(R.id.recipe_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, scrollTo()));
+        onView(withText(NUTELLA_PIE)).perform(click());
         onView(withId(R.id.title_text_view)).check(matches(withText(NUTELLA_PIE)));
     }
 
+    @Test
+    public void clickRecyclerViewItem_Brownies_OpensRecipeDetailActivity() {
+        onView(ViewMatchers.withId(R.id.recipe_list)).perform(RecyclerViewActions.actionOnItemAtPosition(1, scrollTo()));
+        onView(withText(BROWNIES)).perform(click());
+        onView(withId(R.id.title_text_view)).check(matches(withText(BROWNIES)));
+    }
+
+    @Test
+    public void clickRecyclerViewItem_Yellow_Cake_OpensRecipeDetailActivity() {
+        onView(ViewMatchers.withId(R.id.recipe_list)).perform(RecyclerViewActions.actionOnItemAtPosition(2, scrollTo()));
+        onView(withText(YELLOW_CAKE)).perform(click());
+        onView(withId(R.id.title_text_view)).check(matches(withText(YELLOW_CAKE)));
+    }
+
+    @Test
+    public void clickRecyclerViewItem_Cheesecake_OpensRecipeDetailActivity() {
+        onView(ViewMatchers.withId(R.id.recipe_list)).perform(RecyclerViewActions.actionOnItemAtPosition(3, scrollTo()));
+        onView(withText(CHEESECAKE)).perform(click());
+        onView(withId(R.id.title_text_view)).check(matches(withText(CHEESECAKE)));
+    }
 }
